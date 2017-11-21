4eb13dce8faee854a0f7c475f941704036394388
Oleg Khudenko <ohuden@softserveinc.com>
2017-11-21 17:13:55 +0200

    cleared app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './auth/auth.service';
 
 import '../assets/css/styles.css';
 
@@ -8,23 +7,4 @@ import '../assets/css/styles.css';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  email: string;
-  password: string;
-
-  constructor(public authService: AuthService) {}
-
-  signup() {
-    this.authService.signup(this.email, this.password);
-    this.email = this.password = '';
-  }
-
-  login() {
-    this.authService.login(this.email, this.password);
-    this.email = this.password = '';    
-  }
-
-  logout() {
-    this.authService.logout();
-  }
-}
+export class AppComponent { }
